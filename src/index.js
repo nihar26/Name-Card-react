0c3a5387a78a0222c99dcd8bf42b878830cbfc70
niharPersonal <sayyednihar26@gmail.com>
2024-10-17 18:22:57 +0530

    projects changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,39 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const skillList = [
+  {
+    name: "Javascript",
+    Level: "Intermediate",
+    color: "grey",
+  },
+  {
+    name: "Node.js",
+    Level: "Intermediate",
+    color: "Orange",
+  },
+  {
+    name: "React",
+    Level: "begginers",
+    color: "seagreen",
+  },
+  {
+    name: "XSLT",
+    Level: "Intermediate",
+    color: "yellow",
+  },
+  {
+    name: "MarkLogic",
+    Level: "begginers",
+    color: "pink",
+  },
+  {
+    name: "Html/css",
+    Level: "Intermediate",
+    color: "red",
+  },
+];
+
 function App() {
   return (
     <div className="card">
@@ -9,6 +42,7 @@ function App() {
       <div className="data">
         <Intro />
         <Skilllist />
+        <Projects />
       </div>
     </div>
   );
@@ -33,20 +67,62 @@ function Intro() {
 function Skilllist() {
   return (
     <div className="skill-list">
-      <Skill name="Javascript" emoji="💪" color="grey" />
+      {skillList.map((skill) => (
+        <Skill name={skill.name} color={skill.color} Level={skill.Level} />
+      ))}
+      {/* <Skill name="Javascript" emoji="💪" color="grey" />
       <Skill name="Node.js" emoji="💪" color="orange" />
       <Skill name="React" emoji="💪" color="MediumSeaGreen" />
       <Skill name="XSLT" emoji="💪" color="yellow" />
-      <Skill name="Marklogic" emoji="💪" color="pink" />
+      <Skill name="Marklogic" emoji="💪" color="pink" /> */}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ name, color, Level }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.name}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{name}</span>
+      <span>
+        {Level === "begginers" && "👶"}
+        {Level === "Intermediate" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <>
+      <div style={{ "padding-top": "24px" }}>
+        <p>PROJECTS</p>
+      </div>
+      <div className="projcet-list">
+        <Project
+          link="https://github.com/nihar26/shopingCartManagement"
+          name="Shopping Cart"
+        />
+        <Project
+          link="https://github.com/nihar26/protofilio"
+          name="Portfolio"
+        />
+        <Project
+          link="https://github.com/nihar26/eventSchedular"
+          name="Event Handler"
+        />
+        <Project
+          link="https://github.com/nihar26/PizzaMenu-React"
+          name="Pizza Menu"
+        />
+      </div>
+    </>
+  );
+}
+
+function Project({ link, name }) {
+  return (
+    <div className="project">
+      <a href={link}>{name}</a>
     </div>
   );
 }
